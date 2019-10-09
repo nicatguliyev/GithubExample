f5b5e5f13284770a2d0a9cf4cde3c171455b36f4
nicatguliyev <nicatguliyev3@gmail.com>
2019-10-09 23:42:21 +0400

    Initial Commit

---
+++ GithubExample/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  GithubExample
+//
+//  Created by Nicat Guliyev on 10/9/19.
+//  Copyright © 2019 Nicat Guliyev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        print("Salam Eleykim")
+    }
+
+
+}
+
