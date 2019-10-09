e2992bc87edfdc2c3723835b6a960c5312122517
nicatguliyev <nicatguliyev3@gmail.com>
2019-10-09 23:46:06 +0400

    Yeni Controller

---
+++ GithubExample/TestViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TestViewController.swift
+//  GithubExample
+//
+//  Created by Nicat Guliyev on 10/9/19.
+//  Copyright © 2019 Nicat Guliyev. All rights reserved.
+//
+
+import UIKit
+
+class TestViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
